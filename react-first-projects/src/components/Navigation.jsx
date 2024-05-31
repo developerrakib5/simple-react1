23c1c59947bfd512da1b2f8d462042888477c43a
devlrakib <devrakib5@gmail.com>
2024-06-01 02:31:04 +0600

    first commit

---
+++ react-first-projects/src/components/Navigation.jsx
@@ -0,0 +1,25 @@
+
+const navigation = () => {
+    return (
+        <div>
+            <nav>
+                <div className="logo">
+                    <img src="./images/brand_logo.png" alt="logo" />
+                </div>
+                <div>
+                    <ul>
+                        <li href="#">Menu</li>
+                        <li href="#">Location</li>
+                        <li href="#">About</li>
+                        <li href="#">Contact</li>
+                    </ul>
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default navigation;
